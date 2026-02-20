da73fa284a203473d08b961461faae6dc319a5b1
StephenBulldogs <schwe916@d.umn.edu>
2026-02-20 11:28:24 -0600

    Edit Wrap size to 80

--- src/Reservation.java
+++ src/Reservation.java
@@ -48,13 +48,13 @@ public class Reservation {
         this.amenities = amenities;
     }
 
-    public int getAmenityCount() {
-        return amenityCount;
-    }
-
-    public void setAmenityCount(int amenityCount) {
-        this.amenityCount = amenityCount;
-    }
+//    public int getAmenityCount() {
+//        return amenityCount;
+//    }
+//
+//    public void setAmenityCount(int amenityCount) {
+//        this.amenityCount = amenityCount;
+//    }
 
     public int getNights() {
         return nights;
@@ -101,40 +101,29 @@ public class Reservation {
 
     public void printBill() {
         if (checkedIn) {
-            System.out.println(
-                    "Guest must check out before billing.");
+            System.out.println("Guest must check out before billing.");
             return;
         }
 
-        System.out.println("Guest: " +
-                guest.getGuestName());
-        System.out.println("Room: " +
-                room.toString());
-        System.out.println(
-                "---------------------------");
-        System.out.println("Stay (" + nights +
-                " nights): $" +
-                String.format("%.2f",
-                        nights * room.getRoomPricePerNight()));
+        System.out.println("Guest: " + guest.getGuestName());
+        System.out.println("Room: " + room.toString());
+        System.out.println("---------------------------");
+        System.out.println("Stay (" + nights + " nights): $" +
+                String.format("%.2f", nights * room.getRoomPricePerNight()));
 
         for (int i = 0; i < amenityCount; i++) {
-            System.out.println("Amenity: " +
-                    amenities[i].getName() + " ($" +
-                    String.format("%.2f",
-                            amenities[i].getCost()) + ")");
+            System.out.println("Amenity: " + amenities[i].getName() + " ($" +
+                    String.format("%.2f", amenities[i].getCost()) + ")");
         }
 
-        System.out.println(
-                "---------------------------");
+        System.out.println("---------------------------");
         System.out.println("TOTAL DUE: $" +
-                String.format("%.2f",
-                        calculateTotal()));
+                String.format("%.2f", calculateTotal()));
         System.out.println();
     }
 
     @Override
     public String toString() {
-        return guest.toString() + ", " +
-                room.toString();
+        return guest.toString() + ", " + room.toString();
     }
 }
